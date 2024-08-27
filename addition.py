fd66be43b33f2a80ffd40b50cc12e5da14eb33bd
Ashwani <aswanibtech@gmail.com>
2024-08-27 13:27:58 +0530

    fubdmental of addition

---
+++ addition.py
@@ -0,0 +1,17 @@
+#this program is made for tutorial purpose
+'''
+gdjgda
+adaskdhkas
+adaskdjklas
+multiline comment
+'''
+#import math
+#import math as m
+from math import *
+a,b,c=45,45,42
+s=(a+b+c)/2
+print("s=",s)
+#area=math.sqrt(s*(s-a)*(s-b)*(s-c))
+#area=m.sqrt(s*(s-a)*(s-b)*(s-c))
+area=sqrt(s*(s-a)*(s-b)*(s-c))
+print("area=",area)
